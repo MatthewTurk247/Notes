4bd150b2845be1675481d642dc2f2d1d89af8d44
Matthew Turk <mjturk15@gmail.com>
2018-02-27 13:54:49 -0600

    Update notes

--- Lists and List Comprehensions.py
+++ Lists and List Comprehensions.py
@@ -1,4 +1,5 @@
 # Lists
+import random
 
 my_list = ["Abe", "Bev", "Cam", "Dan", "Eve", "Flo", "Gus"]
 my_num_list = [8, 4, 7, 5, 2, 9]
@@ -52,3 +53,71 @@ print(my_list)
 print(my_list.index("Flo"))
 a = my_list.copy()
 print(a)
+
+# add 10 to each item in the list using iteration
+'''for i in range(len(my_list)):
+    my_list[i] += 10
+
+print(my_list)'''
+
+# make a 2d list that is 10x10
+ten_by_ten = list()
+for x in range(10):
+    for y in range(10):
+        my_list.append([x, y])
+        print([x,y])
+
+print("OWEFIUHEWIUFHWEFUIIWE", ten_by_ten)
+
+# print every pair
+for pair in ten_by_ten:
+    print(pair)
+
+for i in range(len(ten_by_ten)):
+    for j in range(len(ten_by_ten[i])):
+        ten_by_ten[i][j] += 3232
+
+# make a list of numbers from 0 to 99 and print it
+num_list = []
+for i in range(100):
+    num_list.append(i)
+
+# square every item in the previous list and print it
+for i in range(len(num_list)):
+    num_list[i] **= 2
+
+# show only odd numbers and print it
+num_list2 = []
+for n in num_list:
+    if n % 2 != 0:
+        print('odd', n)
+        num_list2.append(n)
+
+num_list3 = []
+for i in range(100, 1001):
+    num_list3.append(i)
+
+my_list4 = []
+# do all four at once
+for i in range(100):
+    num = i**2
+    if num % 2 == 1:
+        if num >= 100 and num <= 1000:
+            my_list4.append(num)
+print(my_list4)
+
+# list comprehension
+# [returned_item for iterator in range_or_list filter]
+my_list5 = [x**2 for x in range(100) if x**2 % 2 == 1 and x**2 >= 100 and x**2 <= 1000]
+print(my_list5)
+
+my_rolls = [random.randrange(1, 7) for x in range(101)]
+print(my_rolls)
+my_rolls = [[random.randrange(1, 7), random.randrange(1, 7)] for x in range(101)]
+print(my_rolls)
+doubles = [x for x in my_rolls if x[0] == x[1]]
+print(doubles)
+
+# we can generate a list and filter doubles on a single line
+doubled = [y for y in [[random.randrange(1, 7), random.randrange(1, 7)] for x in range(101)] if y[0] == y[1]]
+print(doubles)
---
+++ Searching.py
@@ -0,0 +1,93 @@
+# Searching
+import re
+
+file = open("data/villains.txt", "r")
+
+for line in file:
+    print(line.strip())
+
+file.close()
+
+file = open("data/villains.txt")
+
+for line in file:
+    print("Hello,", line.strip())
+
+file.close()
+
+'''
+# We can also write to a file
+file = open('data/villains.txt', 'w')
+# overwrites entire file
+
+file.write('Lee The Merciless')
+
+file.close()
+'''
+
+# appending to a file
+'''file = open('data/villains.txt', 'a')
+file.write('\nLee the Merciless')
+file.close()'''
+
+# Read a file into an array
+file = open('data/villains.txt') # the default mode is 'r'
+villains = []
+
+# Linear Search
+
+for name in file:
+    villains.append(name.strip())
+
+file.close()
+key = "Vladimir Noire"
+print(villains)
+# print(villains.index(key))
+i = 0
+while i < len(villains) and villains[i] != key:
+    i += 1
+
+if i < len(villains):
+    print(key, "is at position", i)
+else:
+    print(key, 'is not found')
+
+# Binary Search
+
+key = 'Morgiana the Shrew'
+for i in range(20):
+    print(2 ** i)
+
+lower_bound = 0
+upper_bound = len(villains) - 1
+
+found = False
+middle_pos = 0
+
+while not found and lower_bound <= upper_bound:
+    middle_pos = (upper_bound - lower_bound)//2
+    if villains[middle_pos] < key:
+        lower_bound = middle_pos + 1
+    elif villains[middle_pos] > key:
+        upper_bound = middle_pos - 1
+    else:
+        found = True
+
+if found:
+    print("Found", key, "at position", middle_pos)
+else:
+    print(key, "not found")
+
+# This function takes in a line of text and returns a list of words in the line
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+
+file = open('data/villains.txt')
+all_words = []
+
+for line in file:
+    words = split_line(line)
+    for word in words:
+        all_words.append(word)
+
+print(all_words)
